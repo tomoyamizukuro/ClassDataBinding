4c9b333e91fec6fa0b6c9ce7b2c9aecd472527cf
tomoyamizukuro <salvatoleyohane@gmail.com>
2020-03-20 17:09:35 +0900

    クラスの指定に三項演算子を用いて表示切り替え処理実装

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 var app = new Vue({
   el: "#app",
   data: {
-    largeClass: 'large',
-    dangerClass: 'text-danger',
+    classObject: {
+      large: true,
+      'text-danger': true
+    },
+    largeClass: {
+      large: true,
+      'bg-gray': true
+    },
+    dangerClass: {
+      'text-danger': true
+    },
+    // largeClass: 'large',
+    // dangerClass: 'text-danger',
     isLarge: true,
     hasError: true
   }
